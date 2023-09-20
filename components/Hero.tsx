2032ff03214d532bcc15cc4caad934c846346839
Ashutosh Uniyal <ashutoshuniyal012@gmail.com>
2023-09-20 22:01:09 +0530

    bug fix

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,25 +1,42 @@
-"use client"
+"use client";
 
-import { logo } from "@/assets"
-import Image from 'next/image'
-import ThemeSwitcher from "./ThemeSwitcher"
-import {AiFillGithub} from 'react-icons/ai'
+import { logo } from "@/assets";
+import Image from "next/image";
+import ThemeSwitcher from "./ThemeSwitcher";
+import { AiFillGithub } from "react-icons/ai";
 const Hero = () => {
   return (
-    <header className="w-full flex justify-center items-center flex-col ">
-      <nav className="flex justify-between items-center w-full pt-3 mb-10">
+    <>
+      <div className="w-full top-5 absolute sm:px-20">
+      <nav className="flex justify-between items-center w-full mb-10">
         <Image src={logo} alt="sumz_logo" className="w-28 object-contain" />
         <div className="flex gap-3">
-        <button className="black_btn flex items-center justify-center gap-1" type="button" onClick={()=> window.open("https://github.com/Ashuuniyal12/AI_Summarizer")}>GitHub <AiFillGithub/></button>
-        <ThemeSwitcher/>
+          <button
+            className="black_btn flex items-center justify-center gap-1"
+            type="button"
+            onClick={() =>
+              window.open("https://github.com/Ashuuniyal12/AI_Summarizer")
+            }
+          >
+            GitHub <AiFillGithub />
+          </button>
+          <ThemeSwitcher />
         </div>
       </nav>
+      </div>
 
-      <h1 className="head_text">Summarize Articles With <br className="max-md:hidden" />
-      <span className="orange_gradient">OpenAI GPT-4</span></h1>
-      <h2 className="desc">Simplify your reading with Summize , an open-source article summarizer that transfrom lengthy articles into clear & consise summaries</h2>
-    </header>
-  )
-}
+      <header className="w-full flex justify-center items-center flex-col mt-0 top-0 ">
+        <h1 className="head_text">
+          Summarize Articles With <br className="max-md:hidden" />
+          <span className="orange_gradient">OpenAI GPT-4</span>
+        </h1>
+        <h2 className="desc">
+          Simplify your reading with Summize , an open-source article summarizer
+          that transfrom lengthy articles into clear & consise summaries
+        </h2>
+      </header>
+    </>
+  );
+};
 
-export default Hero
+export default Hero;
